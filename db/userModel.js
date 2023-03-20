4bce04985d904b687d1066f7ef1e716001885247
Kamila Nauryzbayeva <kamila.nryzbv@gmail.com>
2023-03-20 10:43:57 +0600

    Update userModel.js

--- db/userModel.js
+++ db/userModel.js
@@ -20,6 +20,14 @@ const usersSchema = new mongoose.Schema({
     default: null,
   },
   avatarURL: String,
+  verify: {
+    type: Boolean,
+    default: false,
+  },
+  verificationToken: {
+    type: String,
+    required: [true, 'Verify token is required'],
+  },
 });
 
 const Users = mongoose.model("user", usersSchema);
